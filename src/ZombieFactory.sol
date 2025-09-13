eef9c5c34ad0b5bb905461743e55f6fabb161fc6
vfr750y <th91843@proton.me>
2025-09-13 16:31:14 +1200

    add _generateRandomData function

--- src/ZombieFactory.sol
+++ src/ZombieFactory.sol
@@ -16,5 +16,8 @@ contract ZombieFactory {
         zombies.push(Zombie(_name, _dna));
     }
 
-    function _generateRandomDna(string memory _str) private view returns (uint256) {}
+    function _generateRandomDna(string memory _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 }
