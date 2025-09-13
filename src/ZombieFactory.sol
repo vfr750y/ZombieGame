d56f13bd0171c24e28fce210a784ed4997d833e7
vfr750y <th91843@proton.me>
2025-09-13 16:35:50 +1200

    add _generateRandomZombie function

--- src/ZombieFactory.sol
+++ src/ZombieFactory.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+    // declare our event here
+
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10 ** dnaDigits;
 
@@ -14,10 +16,16 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint256 _dna) private {
         zombies.push(Zombie(_name, _dna));
+        // and fire it here
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint256) {
         uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
