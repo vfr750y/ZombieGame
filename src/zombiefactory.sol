bb6ab62b215ca4601e4c04cae4b00d0717b5d57c
vfr750y <th91843@proton.me>
2025-09-23 09:27:32 +1200

    add level and cooldownTime

--- src/zombiefactory.sol
+++ src/zombiefactory.sol
@@ -8,10 +8,13 @@ contract ZombieFactory is Ownable {
 
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10 ** dnaDigits;
+    uint256 cooldownTime = 1 days;
 
     struct Zombie {
         string name;
         uint256 dna;
+        uint32 level;
+        uint32 readytime;
     }
 
     Zombie[] public zombies;
@@ -20,7 +23,7 @@ contract ZombieFactory is Ownable {
     mapping(address => uint256) ownerZombieCount;
 
     function _createZombie(string memory _name, uint256 _dna) internal {
-        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint256 id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
