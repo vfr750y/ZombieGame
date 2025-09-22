364f31d7242eb65cee95388820c181822f1ed3a5
vfr750y <th91843@proton.me>
2025-09-23 09:08:38 +1200

    modify setKittyContractAddress as onlyOwner

--- src/ZombieFeeding.sol
+++ src/ZombieFeeding.sol
@@ -25,7 +25,7 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
 
-    function setKittyContractAddress(address _address) external {
+    function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     }
 
--- src/zombiefactory.sol
+++ src/zombiefactory.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./ownable.sol";
 
-contract ZombieFactory {
+contract ZombieFactory is Ownable {
     event NewZombie(uint256 zombieId, string name, uint256 dna);
 
     uint256 dnaDigits = 16;
