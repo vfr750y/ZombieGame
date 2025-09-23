deacaf2b62b35a1fd4341829d1a856b831562b13
vfr750y <th91843@proton.me>
2025-09-23 19:23:38 +1200

    edit getZombiesByOwner function

--- src/zombiehelper.sol
+++ src/zombiehelper.sol
@@ -17,4 +17,16 @@ contract ZombieHelper is ZombieFeeding {
         require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
+
+    function getZombiesByOwner(address _owner) external view returns (uint256[] memory) {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
 }
